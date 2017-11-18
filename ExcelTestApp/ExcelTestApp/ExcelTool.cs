f199e02d36e9290e9dc3b2853385be5c29605f55
Ivana Tesanovic <i.tesanovic@vegaitsourcing.rs>
2017-11-18 17:28:36 +0100

    ...

--- ExcelTestApp/ExcelTestApp/ExcelTool.cs
+++ ExcelTestApp/ExcelTestApp/ExcelTool.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.IO;
+using System.Linq;
 
 namespace ExcelTestApp
 {
@@ -11,22 +12,29 @@ namespace ExcelTestApp
         private string _excelTemplatePath; //template iz kog vucemo vrednosti za property-e
         private string _excelOriginalPath; //originalni excel fajl koji ce se kopirati svaki put kad se budu export-ovale nove bolesti
         private string _excelDataPath; //fajl u koji ce biti export-ovane bolesti
+        private string _definitionFilePath; //fajl u kome se nalaze opis, sinonimi i sadrzaj, naziv fajla je orpha broj
 
         private Repository _repo;
         private int _numberOfDiseases;
 
-        //TODO: _numberOfDiseases should be sent from the GUI.
+        private List<RowPropertyEntity> RowPropertyValues = new List<RowPropertyEntity>();
+        private List<Disease> _diseases = new List<Disease>();
 
-        //Create a dictionary that will have row number as key (i.e. 2, 3, 4) and property name as value so that the data can be inserted in one pass.
-        //If the data is too big, insert another empty row
+        //TODO: _numberOfDiseases should be sent from the GUI.
 
         public ExcelTool()
         {
-            _excelOriginalPath = "C:\\Users\\i.tesanovic\\Documents\\visual studio 2015\\Projects\\ExcelTestApp\\Data\\RetkeBolestiOriginal.xlsx";
-            _excelTemplatePath = "C:\\Users\\i.tesanovic\\Documents\\visual studio 2015\\Projects\\ExcelTestApp\\Data\\BazaRetkihBolestiTemplate.xlsx";
-            _excelDataPath = "C:\\Users\\i.tesanovic\\Documents\\visual studio 2015\\Projects\\ExcelTestApp\\Data\\RetkeBolesti.xlsx";
+            InitializeList();
+
+            _excelOriginalPath = "E:\\GitHub\\ExcelApp\\ExcelTestApp\\Data\\RetkeBolestiOriginal.xlsx";
+            _excelTemplatePath = "E:\\GitHub\\ExcelApp\\ExcelTestApp\\Data\\BazaRetkihBolestiTemplate.xlsx";
+            _excelDataPath = "E:\\GitHub\\ExcelApp\\ExcelTestApp\\Data\\RetkeBolesti.xlsx";
+            _definitionFilePath = "E:\\GitHub\\ExcelApp\\ExcelTestApp\\Data\\";
+
             _repo = new Repository();
             _numberOfDiseases = 5;
+
+            _diseases = _repo.GetRangeOfDiseases(_numberOfDiseases);
 
             ResetData();
             Console.WriteLine("Data is reset.");
@@ -36,26 +44,36 @@ namespace ExcelTestApp
             Console.WriteLine("Data is exported.");
         }
 
+        //kad dobavim bolesti, mozda odmah da im izracunam i karaktere i sve
+        //napravim neku novu klasu koja ce biti samo za popunjavanje excel-a
+        //i prolazim kroz tu novokreiranu listu i tako popunjavam excel
+        //da ne moram da radim dupli posao
+        //tu listu moram pre svega da popunim da bih znala koliko mi treba redova od svega
+        //znaci od sinonima, sadrzaja i opisa bolesti
+
         //Export to Excel file from Disease object
         //Here we can even have a list of diseases
         //We should export the diseases into an excel file using the given template where each disease will belong to a different sheet
         public void ExportTo(int numberOfDiseases)
         {
-            var diseases = _repo.GetRangeOfDiseases(_numberOfDiseases);
+            for (int i = 1; i <= _diseases.Count; i++)
+            {
+                var disease = _diseases[i - 1];
 
-            InsertDataIntoSheet(1, diseases[0].Name, 2);
-            InsertDataIntoSheet(1, diseases[0].AgeOfOnset, 3);
-            InsertDataIntoSheet(1, diseases[0].Definition, 4);
-
-            //for (int i = 1; i <= diseases.Count; i++)
-            //{
-            //    InsertDataIntoSheet(i, diseases[i - 1].Name);
-            //}
+                foreach (var item in RowPropertyValues)
+                {
+                    var value = disease.GetType().GetProperty(item.PropertyName).GetValue(disease).ToString();
+                    if (item.PropertyName != "Definition")
+                        InsertDataIntoSheet(i, value, item.Row);
+                    else
+                        WriteDefinitionToFile(disease.OrphaNumber, disease.Definition);
+                }
+            }
         }
 
-        //Import from Excel file to Disease object
         public void ImportFrom(object file)
         {
+            //TODO: Import from Excel file to Disease object
         }
 
         private void InitializeExcelDocument(int numberOfSheets)
@@ -122,6 +140,12 @@ namespace ExcelTestApp
             }
         }
 
+        //kad popunjavam property-e, to mogu da radim sve u istom cugu, da odmah dobavim bolesti
+        //i na osnovu toga znam koliko puta treba da dodam red za sinonime, sadrzaj i za opis, jer toga ima najvise
+        //znaci ako iz sinonima izvucem 3 vrednosti, znaci 3 reda
+        //a pre svakog upisa treba da proverim da li odgovarajuca vrednost ima vise od 255 karaktera
+        //da bih mogla da upisem onda to u vise redova
+
         private List<string> GetPropertyNames()
         {
             List<string> propertyNames = new List<string>();
@@ -151,6 +175,78 @@ namespace ExcelTestApp
             return dataSet;
         }
 
+        //private List<ExcelDiseaseEntity> InitializeDiseases()
+        //{
+        //    List<ExcelDiseaseEntity> excelDiseases = new List<ExcelDiseaseEntity>();
+
+        //    foreach(var disease in _diseases)
+        //    {
+        //        foreach(var property in disease.GetType().GetProperties())
+        //        {
+        //            var rowIndex = RowPropertyValues.Where(v => v.PropertyName == property.Name).FirstOrDefault() == null? 0 : RowPropertyValues.Where(v => v.PropertyName == property.Name).First().Row;
+
+        //            if (property.Name != "Summaries" && property.Name != "Synonyms")
+        //            {
+        //                var propertyValue = property.GetValue(disease).ToString();
+
+        //                if (propertyValue.Length > 255)
+        //                {
+        //                    //ovde treba duzinu podeliti sa 255 da bih znala koliko da ubacim redova
+        //                    var numberOfRows = property.GetValue(disease).ToString().Length % 255 != 0 ? property.GetValue(disease).ToString().Length % 255 + 1 : property.GetValue(disease).ToString().Length % 255;
+        //                    for(int i = 0; i < numberOfRows; i++)
+        //                        excelDiseases.Add(new ExcelDiseaseEntity { NumberOfRows = numberOfRows, PropertyName = property.Name, RowIndex = rowIndex });
+        //                }
+        //                else
+        //                {
+        //                    //ako nije veca od 255 samo ubaciti jedan red
+        //                    //rowindex cemo izvuci iz ove liste, mozda bolje da napravim dictionary gde je property name key i onda samo uzmem row index kao vrednost
+        //                    //na sta ovo lici, ivana, pobogu
+        //                    excelDiseases.Add(new ExcelDiseaseEntity { NumberOfRows = 1, PropertyName = property.Name, PropertyValue = propertyValue, RowIndex = rowIndex });
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return excelDiseases;
+        //}
+
+        //trebace se posle svaki rowindex update-ovati u zavisnosti od naziva property-a
+
+        private void InitializeList()
+        {
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 2, PropertyName = "Name" });
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 3, PropertyName = "" }); //skracen naziv bolesti
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 4, PropertyName = "" }); //sinonimi
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 5, PropertyName = "" }); //kategorija
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 6, PropertyName = "" }); //podkategorija
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 7, PropertyName = "Name" }); //naziv na engleskom jeziku
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 8, PropertyName = "OrphaNumber" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 9, PropertyName = "Prevalence" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 10, PropertyName = "Inheritance" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 11, PropertyName = "AgeOfOnset" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 12, PropertyName = "Icd10" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 13, PropertyName = "Omim" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 14, PropertyName = "Umls" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 15, PropertyName = "MeSH" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 16, PropertyName = "Gard" });
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 17, PropertyName = "MedDra" });
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 18, PropertyName = "" }); //sadrzaj? to ne znam kako cemo
+            RowPropertyValues.Add(new RowPropertyEntity { Row = 19, PropertyName = "Definition" });
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 20, PropertyName = "" }); //dijagnostika
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 21, PropertyName = "" }); //terapije
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 22, PropertyName = "" }); //validacije lekara
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 23, PropertyName = "" }); //prognoze
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 24, PropertyName = "" }); //klinicka istrazivanja
+            //RowPropertyValues.Add(new RowPropertyEntity { Row = 25, PropertyName = "" }); //dodatni clanci, linkovi, komentari
+        }
+
+        private void WriteDefinitionToFile(string orphaNumber, string definition)
+        {
+            // Create a file to write to.
+            //string createText = "Hello and Welcome" + Environment.NewLine;
+            File.WriteAllText($"{_definitionFilePath}{orphaNumber}.txt", definition);
+        }
+
         private string GetConnectionString(string filePath, string hdr) => $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath};Extended Properties='Excel 8.0;HDR={hdr};'";
 
         private void ResetData()
@@ -161,6 +257,12 @@ namespace ExcelTestApp
             }
 
             File.Copy(_excelOriginalPath, _excelDataPath);
+        }
+
+        private class RowPropertyEntity
+        {
+            public int Row { get; set; }
+            public string PropertyName { get; set; }
         }
     }
 }
